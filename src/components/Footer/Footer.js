4f42a55b022ce7a631ac28659e5c55ee716a67b8
Muhammad Shahbaz <rssialvi@gmail.com>
2020-10-13 21:07:00 +0500

    Updated footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,38 +8,38 @@ function Footer() {
             <div className={style.endposter}>
                 <Container-Fluid className={style.endposter}>
                     <Row>
-                        <Col>POPULAR CATEGORIES</Col>
-                        <Col>TRENDING SEARCHES</Col>
-                        <Col>ABOUT US</Col>
-                        <Col>OLX</Col>
-                        <Col>FOLLOW US</Col>
+                        <Col className={style.heading}>POPULAR CATEGORIES</Col>
+                        <Col className={style.heading}>TRENDING SEARCHES</Col>
+                        <Col className={style.heading}>ABOUT US</Col>
+                        <Col className={style.heading}>OLX</Col>
+                        <Col className={style.heading}>FOLLOW US</Col>
                     </Row>
                     <Row>
-                        <Col>Cars</Col>
-                        <Col>Bikes</Col>
-                        <Col>About OLX Group</Col>
-                        <Col>Help</Col>
+                        <Col className={style.xs}>Cars</Col>
+                        <Col className={style.xs}>Bikes</Col>
+                        <Col className={style.xs}>About OLX Group</Col>
+                        <Col className={style.xs}>Help</Col>
                         <Col><img src=""></img><img src=""></img><img src=""></img><img src=""></img><img src=""></img></Col>
                     </Row>
                     <Row>
-                        <Col>1</Col>
-                        <Col>2</Col>
-                        <Col>3</Col>
-                        <Col>4</Col>
+                        <Col className={style.xs}>Flats for rent</Col>
+                        <Col className={style.xs}>Watches</Col>
+                        <Col className={style.xs}>Olx blog</Col>
+                        <Col className={style.xs}>sitemap</Col>
                         <Col>5</Col>
                     </Row>
                     <Row>
-                        <Col>6</Col>
-                        <Col>7</Col>
-                        <Col>8</Col>
-                        <Col>9</Col>
+                        <Col className={style.xs}>Jobs</Col>
+                        <Col className={style.xs}>Books</Col>
+                        <Col className={style.xs}>Contact Us</Col>
+                        <Col className={style.xs}>Legal & Privacy information</Col>
                         <Col>0</Col>
                     </Row>
                     <Row>
-                        <Col>1</Col>
-                        <Col>2</Col>
-                        <Col>3</Col>
-                        <Col>4</Col>
+                        <Col className={style.xs}>Mobile Phones</Col>
+                        <Col className={style.xs}>Dogs</Col>
+                        <Col className={style.xs}>OLX for Businesses</Col>
+                        <Col className={style.xs}></Col>
                         <Col>5</Col>
                     </Row>
                 </Container-Fluid>
